5a9c19269acff0e6e16097bc06c6933311a1dcba
kimmin <kimmin2301@gmail.com>
2023-03-27 19:56:52 +0900

    feat: v1 log tracer

---
+++ src/main/java/hello/advanced/app/v1/v0/OrderServiceV1.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v1.v0;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.hellotrace.HelloTraceV1;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OrderServiceV1 {
+
+    private final HelloTraceV1 trace;
+    private final OrderRepositoryV1 orderRepository;
+
+    public void orderItem(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderService.orderItem()");
+
+            orderRepository.save(itemId);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+            throw e; // 예외를 꼭 다시 던져줘야 한다.
+        }
+    }
+}
